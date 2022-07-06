080ec49af571b38abb445cc603e0717f13bb4c7c
Imran8168 <shaikhimran8168@gmail.com>
2022-07-06 20:39:57 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
+
+import { useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
+import TodoList from './components/TodoList';
+
 
 function App() {
+
+  const [theme,setTheme] = useState("light-theme")
+
+const togglefun =()=>{
+  if(theme === "dark-theme"){
+    setTheme('light-theme');
+  }
+  else{
+    setTheme("dark-theme")
+  }
+}
+
+useEffect(()=>{
+  document.body.className = theme;
+},[theme])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='togglebtn'><button onClick={()=>togglefun()} className="togbtn">Toggle Mode</button></div>
+
+    <div className="todo-app">
+     <h1>Todo App</h1>
+     <TodoList />
     </div>
+    </>
   );
 }
 
---
+++ src/components/TodoForm.js
@@ -0,0 +1,37 @@
+import React, { useState,useEffect,useRef } from 'react'
+
+function TodoForm(props) {
+    const [input,setInput] = useState("");
+
+    const inputRef = useRef(null);
+
+    useEffect(()=>{
+        inputRef.current.focus()
+    })
+    
+    const handleChange =(e)=>{
+        setInput(e.target.value)
+    }
+
+    const handelSubmit = (e) =>{
+        e.preventDefault();
+        
+        props.onSubmit({
+            id:Math.floor(Math.random() * 10000),
+            text:input
+        })
+
+        setInput('')
+    }
+
+  return (
+    <form className='todo-form' onSubmit={handelSubmit}>
+
+        <input type="text" placeholder='Add a todo' value={input} name="text" className='todo-input' onChange={handleChange} ref={inputRef}/>
+
+        <button className='todo-button'>Add to do</button>
+    </form>
+  )
+}
+
+export default TodoForm
